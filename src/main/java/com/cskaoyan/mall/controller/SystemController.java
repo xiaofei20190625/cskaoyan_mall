c92ffa82d1c61bd89b7077beda130a8b468718e8
lcsdxss <zzuxxy@163.com>
2019-07-03 21:09:43 +0800

    by xiyuan

---
+++ src/main/java/com/cskaoyan/mall/controller/SystemController.java
@@ -0,0 +1,13 @@
+package com.cskaoyan.mall.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SystemController {
+
+/**   items: [{id: 12, username: "cskaoyan",…}, {id: 6, username: "123213213",…},…]
+     total: 4
+     errmsg: "成功"
+     errno: 0/*/
+
+}

2f99ba2bc6a80c75064fc9e7dcc25258df02084c
AlexChengQ <ahtcmcq940603@163.com>
2019-07-04 09:44:26 +0800

    'ad'

---
+++ src/main/java/com/cskaoyan/mall/mapper/AdMapper.java
@@ -0,0 +1,21 @@
+package com.cskaoyan.mall.mapper;
+
+import com.cskaoyan.mall.bean.Ad;
+
+import java.util.List;
+
+public interface AdMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Ad record);
+
+    int insertSelective(Ad record);
+
+    Ad selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Ad record);
+
+    int updateByPrimaryKey(Ad record);
+
+    List<Ad> selectPageAd(int page, int limit, String sort, String order);
+}

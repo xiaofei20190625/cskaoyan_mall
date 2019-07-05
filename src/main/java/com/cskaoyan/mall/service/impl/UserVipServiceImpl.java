52fab89148e3ff38cd1e679d5b082fcdeb96ccbf
xiaofei20190625 <981962598@qq.com>
2019-07-05 21:56:02 +0800

    xiaofei

--- src/main/java/com/cskaoyan/mall/service/impl/UserVipServiceImpl.java
+++ src/main/java/com/cskaoyan/mall/service/impl/UserVipServiceImpl.java
@@ -1,13 +1,10 @@
 package com.cskaoyan.mall.service.impl;
 
+import com.cskaoyan.mall.bean.PageList;
 import com.cskaoyan.mall.bean.UserVip;
 import com.cskaoyan.mall.mapper.UserVipMapper;
 import com.cskaoyan.mall.service.UserVipService;
-import com.cskaoyan.mall.utils.PageUtil;
 import com.cskaoyan.mall.vo.PageVO;
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +19,7 @@ public class UserVipServiceImpl implements UserVipService {
     @Override
     public PageVO<UserVip> findUserVipPageByUsernameAndMobile(int page, int limit, String username, String mobile, String sort, String order) {
         List<UserVip> userVips = userVipMapper.selectUserVipByUsernameAndMobile(username, mobile, sort ,order);
-        PageVO<UserVip> pagevo = new PageUtil<UserVip>().pagevo(page, limit, userVips);
+        PageVO<UserVip> pagevo = new PageList<UserVip>().pagevo(page, limit, userVips);
         return pagevo;
     }
 }

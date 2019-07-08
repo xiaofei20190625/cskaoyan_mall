848d6c98b937392937a01f1f5d85b2e1705644ce
LeeYaonan <leeyaonan@outlook.com>
2019-07-08 19:38:05 +0800

    lyn

---
+++ src/main/java/com/cskaoyan/mall/wx/bean/UserInfo.java
@@ -0,0 +1,11 @@
+package com.cskaoyan.mall.wx.bean;
+
+/**
+ * Created by IceFloe_Rot
+ * Date 2019/7/8 Time 17:58
+ */
+public class UserInfo {
+    private String avatarUrl;
+    private String nickname;
+
+}
---
+++ src/main/java/com/cskaoyan/mall/wx/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.cskaoyan.mall.wx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by IceFloe_Rot
+ * Date 2019/7/8 Time 17:40
+ */
+@Controller
+@RequestMapping("/wx/auth")
+public class LoginController {
+
+
+
+    @RequestMapping("login")
+    @ResponseBody
+    public
+}
